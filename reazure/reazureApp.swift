0bb8653cfee9ba287b36017fc9694e6732ea599b
Gyuhwan Park★ <unstabler@unstabler.pl>
2024-11-02 19:37:57 +0900

    Initial Commit

---
+++ reazure/reazureApp.swift
@@ -0,0 +1,17 @@
+//
+//  reazureApp.swift
+//  reazure
+//
+//  Created by cheesekun on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct reazureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
