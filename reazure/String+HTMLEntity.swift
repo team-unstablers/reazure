367f137a8db94a1d0a9ad88bb4f221cc9cceb179
Gyuhwan Park★ <unstabler@unstabler.pl>
2024-11-12 16:19:46 +0900

    timeline: iOS에서 String+decodeHTMLEntity()가 동작하지 않아 직접 구현함

--- reazure/String+HTMLEntity.swift
+++ reazure/String+HTMLEntity.swift
@@ -7,18 +7,120 @@
 
 import Foundation
 
+fileprivate let ENTITY_TABLES = [
+    "amp": "&",
+    "lt": "<",
+    "gt": ">",
+    
+    "nbsp": " ",
+    "iexcl": "¡",
+    "cent": "¢",
+    "pound": "£",
+    "curren": "¤",
+    "yen": "¥",
+    "brvbar": "¦",
+    "sect": "§",
+    "uml": "¨",
+    "copy": "©",
+    "ordf": "ª",
+    "laquo": "«",
+    "not": "¬",
+    "shy": "­",
+    "reg": "®",
+    "macr": "¯",
+    "deg": "°",
+    "plusmn": "±",
+    "sup2": "²",
+    "sup3": "³",
+    "acute": "´",
+    "micro": "µ",
+    "para": "¶",
+    "cedil": "¸",
+    "sup1": "¹",
+    "ordm": "º",
+    "raquo": "»",
+    "frac14": "¼",
+    "frac12": "½",
+    "frac34": "¾",
+    "iquest": "¿",
+    "times": "×",
+    "divide": "÷",
+    
+    "forall": "∀",
+    "part": "∂",
+    "exist": "∃",
+    "empty": "∅",
+    "nabla": "∇",
+    "isin": "∈",
+    "notin": "∉",
+    "ni": "∋",
+    "prod": "∏",
+    "sum": "∑",
+    "minus": "−",
+    "lowast": "∗",
+    "radic": "√",
+    "prop": "∝",
+    "infin": "∞",
+    "ang": "∠",
+    "and": "∧",
+    "or": "∨",
+    "cap": "∩",
+    "cup": "∪",
+    "int": "∫",
+    "there4": "∴",
+    "sim": "∼",
+    "cong": "≅",
+    "asymp": "≈",
+    "ne": "≠",
+    "equiv": "≡",
+    "le": "≤",
+    "ge": "≥",
+    "sub": "⊂",
+    "sup": "⊃",
+    "nsub": "⊄",
+    "sube": "⊆",
+    "supe": "⊇",
+    "oplus": "⊕",
+    "otimes": "⊗",
+    "perp": "⊥",
+    "sdot": "⋅",
+]
+
 extension String {
     func decodeHTMLEntity() -> String {
-        let whitespacePrefix = self.prefix { $0 == " " }
+        let scanner = Scanner(string: self)
+        scanner.charactersToBeSkipped = nil
         
-        let encodedData = self.data(using: .utf8)!
-        let attributedOptions: [NSAttributedString.DocumentReadingOptionKey: Any] = [
-            .documentType: NSAttributedString.DocumentType.html,
-            .characterEncoding: String.Encoding.utf8.rawValue
-        ]
-        let attributedString = try! NSAttributedString(data: encodedData, options: attributedOptions, documentAttributes: nil)
+        var result = ""
         
-        // whitespace를 복원
-        return (whitespacePrefix + attributedString.string)
+        while !scanner.isAtEnd {
+            if let string = scanner.scanUpToString("&") {
+                result.append(string)
+            }
+            
+            if scanner.scanString("&") != nil {
+                if let entity = scanner.scanUpToString(";") {
+                    if entity.hasPrefix("#x") {
+                        if let code = UInt32(entity.dropFirst(2), radix: 16) {
+                            result.append(String(UnicodeScalar(code)!))
+                        }
+                    } else if entity.hasPrefix("#") {
+                        if let code = UInt32(entity.dropFirst(), radix: 10) {
+                            result.append(String(UnicodeScalar(code)!))
+                        }
+                    } else {
+                        if let decoded = ENTITY_TABLES[entity] {
+                            result.append(decoded)
+                        } else {
+                            result.append("&\(entity);")
+                        }
+                    }
+                }
+                
+                scanner.scanString(";")
+            }
+        }
+        
+        return result
     }
 }
