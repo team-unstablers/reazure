e3da444a0ab0e6429c4d14b9915ba467dbb95030
Gyuhwan Park★ <unstabler@unstabler.pl>
2024-11-16 14:40:16 +0900

    views/timeline: Model이 가지고 있는 StatusAdaptor를 mutable하게 변경

--- reazure/fediverse/models/StatusModel.swift
+++ reazure/fediverse/models/StatusModel.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 class StatusModel: ObservableObject {
-    let status: StatusAdaptor
+    @Published
+    var status: StatusAdaptor
     
     @Published
     var parents: [StatusAdaptor] = []
