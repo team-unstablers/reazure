88404832a616767e17c5e136f605d07917eda17a
Gyuhwan Park★ <unstabler@unstabler.pl>
2024-11-24 20:10:08 +0900

    assets: Cansol님의 멋진 알림 소리 추가

--- reazure/assets/notification_sounds/NotificationSound+Licenses.swift
+++ reazure/assets/notification_sounds/NotificationSound+Licenses.swift
@@ -8,6 +8,8 @@
 extension NotificationSound {
     var description: String {
         switch self {
+        case .reazure:
+            return "re;azure 기본 알림 소리"
         case .notify32:
             return "어째선지 그리움이 느껴지는 소리"
         case .boop:
@@ -19,6 +21,8 @@ extension NotificationSound {
     
     var copyright: String {
         switch self {
+        case .reazure:
+            "2024 Cansol (https://soundcloud.com/cansol)"
         case .notify32:
             "2024 Gyuhwan Park⭐️ (@cheesekun@ppiy.ac)"
         case .boop:
@@ -30,6 +34,8 @@ extension NotificationSound {
     
     var license: String {
         switch self {
+        case .reazure:
+            "CC0"
         case .notify32:
             "CC0"
         case .boop:
