c70948552ad673a7d86ee1cf1d469719f99a6ed4
Gyuhwan Park★ <unstabler@unstabler.pl>
2024-11-30 15:52:32 +0900

    checkpoint(api/models): SharedClient의 숏컷 핸들러 관련 기능을 StatusModel, TimelineModel로 분리 및 이관

--- reazure/fediverse/models/StatusModel.swift
+++ reazure/fediverse/models/StatusModel.swift
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
-class StatusModel: ObservableObject {
+
+class StatusModel: StatusModelBase, ObservableObject {
+    weak var performer: Performer?
+    
     @Published
     var status: StatusAdaptor
     
@@ -23,6 +26,8 @@ class StatusModel: ObservableObject {
     init(adaptor status: StatusAdaptor) {
         self.status = status
     }
+    
+
 }
 
 extension StatusModel: Hashable, Equatable, Identifiable {
@@ -36,37 +41,5 @@ extension StatusModel: Hashable, Equatable, Identifiable {
     
     var id: String {
         return status.id
-    }
-}
-
-extension StatusModel {
-    func resolveParent(of status: StatusAdaptor, using client: MastodonClient) {
-        guard let parentId = (status.reblog ?? status).replyToId else {
-            return
-        }
-        
-        if (self.resolving) {
-            return
-        }
-        
-        self.resolving = true
-        
-        
-        Task {
-            defer {
-                DispatchQueue.main.async {
-                    self.resolving = false
-                }
-            }
-            do {
-                let parent = try await client.status(of: parentId)
-                
-                DispatchQueue.main.async {
-                    self.parents.append(MastodonStatusAdaptor(from: parent))
-                }
-            } catch {
-                print("Failed to resolve parent: \(error)")
-            }
-        }
     }
 }
